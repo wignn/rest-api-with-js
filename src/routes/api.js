b64057d761760586798df0c088885266c343dd8b
wignn <tigfiragnafatur1933@gmail.com>
2024-10-18 10:30:06 +0700

    Refactor user and mail routes in api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,12 +5,12 @@ import { mailController } from '../controller/mail-controller.js';
 export const apiRouter = express.Router();
 
 
+// user routes
 apiRouter.post("/api/users", userController.create);
 apiRouter.patch("/api/users/login", userController.login);
 apiRouter.get("/api/users/:username", userController.get);
 apiRouter.put("/api/users/:username", userController.update);
 apiRouter.delete("/api/users/:username", userController.delete);
 
-
-//
+// mailer routes
 apiRouter.post("/api/mailer/current", mailController.create)
